cbbb9f70ddbd723cf4638441b39ccc1f19b6e1ae
Hosi121 <takuyakataiwa@icloud.com>
2024-08-27 19:58:02 +0900

    feat: main.goを定義

---
+++ backend/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	// 環境変数をロード
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	r := gin.Default()
+
+	// CORS ミドルウェアを適用
+	r.Use(middleware.CORSMiddleware())
+
+	models.ConnectDatabase()
+
+	// データベース接続を設定
+	models.SetDatabase(models.DB)
+
+	// ルートを設定
+	routes.AuthRoutes(r)
+
+	// ポートを指定してサーバーを起動
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8081"
+	}
+	r.Run(":" + port)
+}
