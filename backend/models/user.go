319caece059ab0b0463a03a04a1c1b2f7de60682
Hosi121 <takuyakataiwa@icloud.com>
2024-08-27 22:08:58 +0900

    fix: frontendと調整

--- backend/models/user.go
+++ backend/models/user.go
@@ -17,7 +17,7 @@ type User struct {
 type RegisterInput struct {
 	Username string `json:"username" binding:"required"`
 	Email    string `json:"email" binding:"required,email"`
-	Password string `json:"password" binding:"required,min=8"`
+	Password string `json:"password" binding:"required,min=6"`
 }
 
 type LoginInput struct {
