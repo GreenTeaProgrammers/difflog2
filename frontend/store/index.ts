c8ea542f84608f0d5ddfd5a7f7d3e3943e881ae6
Hosi121 <takuyakataiwa@icloud.com>
2024-08-28 18:28:24 +0900

    fix: import関連のエラーを修正

--- frontend/store/index.ts
+++ frontend/store/index.ts
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { useDispatch } from 'react-redux';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import rootReducer from './rootReducer';
 
 export const store = configureStore({
@@ -10,4 +10,4 @@ export const store = configureStore({
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
 export const useAppDispatch = () => useDispatch<AppDispatch>();
-
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
