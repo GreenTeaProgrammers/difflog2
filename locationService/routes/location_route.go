63cf1976cb61294368e50a16348a39f3892c07c3
Hosi121 <takuyakataiwa@icloud.com>
2024-08-30 05:03:54 +0900

    feat : location_routeの追加

---
+++ locationService/routes/location_route.go
@@ -0,0 +1,19 @@
+package routes
+
+import (
+	"github.com/GreenTeaProgrammers/difflog2/locationService/controllers"
+	"github.com/GreenTeaProgrammers/difflog2/locationService/models"
+	"github.com/gin-gonic/gin"
+)
+
+func RegisterLocationRoutes(router *gin.Engine) {
+	locationController := controllers.NewLocationController(models.DB)
+
+	locationRoutes := router.Group("/locations")
+	{
+		locationRoutes.POST("", locationController.CreateLocation)
+		locationRoutes.GET("/:id", locationController.GetLocation)
+		locationRoutes.PUT("/:id", locationController.UpdateLocation)
+		locationRoutes.DELETE("/:id", locationController.DeleteLocation)
+	}
+}
