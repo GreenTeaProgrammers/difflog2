52f17f7448a0dc0e9f8c11f7beac5c88aa1057f4
Hosi121 <takuyakataiwa@icloud.com>
2024-08-30 14:24:04 +0900

    feat: commitのrouteを定義

---
+++ commitService/routes/commit_routes.go
@@ -0,0 +1,22 @@
+package routes
+
+import (
+	"github.com/GreenTeaProgrammers/difflog2/commitService/controllers"
+
+	"github.com/GreenTeaProgrammers/difflog2/commitService/models"
+	"github.com/gin-gonic/gin"
+)
+
+func CommitRoutes(router *gin.Engine) {
+	commitController := controllers.CommitController{DB: models.DB}
+
+	commitGroup := router.Group("/commits")
+	{
+		commitGroup.POST("/", commitController.CreateCommit)
+		commitGroup.GET("/", commitController.GetCommits)
+		commitGroup.GET("/:id", commitController.GetCommit)
+		commitGroup.PUT("/:id", commitController.UpdateCommit)
+		commitGroup.DELETE("/:id", commitController.DeleteCommit)
+		commitGroup.GET("/count_by_date", commitController.GetCommitCountByDate) // 同日付のコミット数を返却
+	}
+}
